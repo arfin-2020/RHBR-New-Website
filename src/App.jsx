085db1f957934aadda7c036bb17167dc452f6ccd
Arfin Chowdhury Arif <arfinchowdhuryarif7@gmail.com>
2026-04-14 13:00:35 +0400

    Integrated React-GA4 for detailed tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import React, { useEffect } from 'react';
+import ReactGA from "react-ga4";
 import Home from "./Pages/Home";
 import MenuPage from "./Pages/MenuPage";
 import AboutUs from "./Pages/AboutUS";
@@ -11,7 +12,16 @@ import ReservationSection from "./Pages/ReservationForm";
 import ScrollToTop from "./Pages/ScrollToTop";
 import { Nav } from "./Pages/Nav";
 
+
+const TRACKING_ID = "G-Y8VGR9PSST"; 
+ReactGA.initialize(TRACKING_ID);
+
+
 function App() {
+  useEffect(() => {
+    // This tracks the initial page load
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
   return (
     <>
       <ScrollToTop />
